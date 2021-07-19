52a8a06e8d9e3e073ec063c184e774c536732866
Kang-hg <85867838+Kang-hg@users.noreply.github.com>
2021-07-19 17:41:48 +0900

    Add files via upload

---
+++ Py0719/ex03_0719.py
@@ -0,0 +1,5 @@
+import time
+
+print("지금부터 3.5초 동안 정지합니다!")
+time.sleep(3.5)
+print("프로그램을 종료합니다")
---
+++ Py0719/ex17_0719.py
@@ -0,0 +1,13 @@
+# 클래스를 선언합니다.
+class Student:
+    def __init__(self):
+        pass
+
+# 학생을 선언합니다.
+student = Student()
+
+# 인스턴스 확인하기
+print()
+print("isinstance(student, Student):", isinstance(student, Student))
+print("type(student):", type(student) == Student)
+print()
